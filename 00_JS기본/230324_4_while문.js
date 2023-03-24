d210ea2852885c0c1866302c734f31764a45acfa
양경미 <ygm815@naver.com>
2023-03-24 15:10:29 +0900

    .

--- 00_JS기본/230324_4_while문.js
+++ 00_JS기본/230324_4_while문.js
@@ -1,15 +1,27 @@
-let treeHit = 0;
+// let treeHit = 0;
 // while(treeHit < 10) {
 //   treeHit++;
 //   document.write(`<p>나무를 ${treeHit}번 찍었습니다.</p>`);
 //   if(treeHit == 10) document.write(`<p>나무가 넘어 갑니다.</p>`);
 // }
 
+// while(true) {
+//   treeHit++;
+//   document.write(`<p>나무를 ${treeHit}번 찍었습니다.</p>`);
+//   if(treeHit == 10) {
+//     document.write(`<p>나무가 넘어 갑니다.</p>`);
+//     break;
+//   }
+// }
+
+let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5); // 조건이 참인 동안 수행
+
 while(true) {
-  treeHit++;
-  document.write(`<p>나무를 ${treeHit}번 찍었습니다.</p>`);
-  if(treeHit == 10) {
-    document.write(`<p>나무가 넘어 갑니다.</p>`);
-    break;
-  }
+  console.log(i);
+  i++;
+  if(i > 5) break;
 }
