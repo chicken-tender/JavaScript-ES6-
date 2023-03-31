4e2e3b5b920bb5dc39743c23d68fe651d2e3575a
chicken-tender <ygm815@naver.com>
2023-04-01 08:30:26 +0900

    수정

--- 01_JS응용/230331_2_0_이벤트등록.js
+++ 01_JS응용/230331_2_0_이벤트등록.js
@@ -15,9 +15,8 @@ function blurEvent() {
 
 // ⭐️ 2. 이벤트 등록 메소드 방식
 // <노드>.addEventListener("이벤트타입", 함수);
-let regBtnEl = document.getElementById("reg_btn");
-regBtnEl.addEventListener("click", eventFunc); // 콜백함수
-
 function eventFunc() { // 네이밍 함수는 호이스팅이 되기 때문에 뒤에 선언해도 호출 가능
   alert("이벤트 등록 메소드로 구현된 함수를 호출");
 }
+let regBtnEl = document.getElementById("reg_btn");
+regBtnEl.addEventListener("click", eventFunc); // 콜백함수
