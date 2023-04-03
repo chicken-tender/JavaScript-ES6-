41b9e91d7b3fd8e55d29e2ec92315706cd11c3ae
chicken-tender <ygm815@naver.com>
2023-04-03 14:31:04 +0900

    예매 완료시 알림창 추가

--- 01_JS응용/230403_3_영화표예매.js
+++ 01_JS응용/230403_3_영화표예매.js
@@ -1,10 +1,9 @@
-/* 메뉴는 [1] 예매하기, [2] 종료하기
+/* 메뉴는 [1] 예매하기, [2] 정산하기
 - 사용자로부터 좌석번호(index)를 입력받아 예매하는 시스템이다. (좌석은 10개이다.)
 - [V] [V] [V] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-- 예매가 완료되면 해당 좌석 값을 1로 변경한다.
 - 이미 예매가 완료된 좌석은 재구매 할 수 없다.
 - 한 좌석당 예매 가격은 12000원
-- 프로그램 종료 후, 해당 영화관의 총 매출액 출력 */
+- 정산하기 클릭시 해당 영화관의 총 매출액 출력 */
 
 const reserveEl = document.querySelector("#reserve");
 reserveEl.addEventListener("click", function() {
@@ -22,8 +21,9 @@ reserveBtn.addEventListener("click", function() {
   for(let e of seat) {
     let tmp = e.className.substring(10);
     if(tmp == seatNum) {
-      if(e.style.backgroundColor == "royalblue") alert("이미 예약된 좌석 입니다.");
+      if(e.style.backgroundColor == "royalblue") alert("이미 예매가 완료된 좌석 입니다.");
       e.style.backgroundColor = "royalblue";
+      alert("예매가 완료되었습니다.");
       const inputBox = document.querySelector("#seatNum");
       inputBox.value = "";
       document.getElementById("seatNum").focus();
