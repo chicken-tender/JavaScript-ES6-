eec5c367a75e67efb7595fa03175fb401d6db0b7
chicken-tender <ygm815@naver.com>
2023-04-08 16:44:21 +0900

    수정

--- 02_JS_HTTP와RESTful/230406_1_비동기통신_XMLHttpRequest.js
+++ 02_JS_HTTP와RESTful/230406_1_비동기통신_XMLHttpRequest.js
@@ -1,7 +1,7 @@
 // XMLHttpRequest()는 타임 아웃 걸 때가 없어서 서버에서 응답 없으면 무한대기 걸려버리는 단점이 있다.)
 const xhr = new XMLHttpRequest(); // XMLHttpRequest 객체 생성
-xhr.open("POST", "https://jsonplaceholder.typicode.com/posts"); // HTTP Method, URI??
-xhr.setRequestHeader("content-type", "application/json"); // 헤더 값 중 content-type 정의
+xhr.open("POST", "https://jsonplaceholder.typicode.com/posts"); // HTTP Method, URI
+xhr.setRequestHeader("content-type", "application/json"); // 헤더 값 content-type 정의
 const data = {
   // 아래 변수들은 추후에는 input에서 받아온 값(~.value)이 들어감.
   id : "test1",
@@ -17,7 +17,7 @@ xhr.send(JSON.stringify(data)); // XMLHttpRequest 객체를 사용하여 서버�
 xhr.onload = () => { // onload() : 로딩이 되면 ...(서버가 응답을 주면) 'onload()'가 불려지는..콜백 함수
   // XMLHttpRequest 객체에서 요청이 공적으로 완료되었을 때 호출되는 핸들러
   if (xhr.status === 201) {
-    // 정상적으로 응답되면 status가 200
+    // 정상적으로 응답되면 status가 200이나 해당 test 사이트는 201이 정상 응답인가 봄.
     // const res = JSON.parse(xhr.response); // 응답 데이터를 JSON.parse 함수로 JSON 객체로 변경
     console.log(xhr.response);
   } else {
---
+++ promise.js
@@ -0,0 +1,36 @@
+// 새로운 Promise를 만들 때, executor 함수가 자동적으로 실행되기 때문에 사용자가 필요할 때에만 통신이 이루어져야 할 때에는 적합하지 않음.
+// 즉 불필요한 네트워크 통신을 하게 될 수 있음
+const promise = new Promise((resolve, reject) => {
+  // 비동기 작업()...(네트워크, 파일 읽기 등)
+  console.log("작업중...");
+  setTimeout(() => {
+    // resolve('kongmi');
+    reject(new Error("네트워크가 없습니다."));
+  }, 2000);
+});
+
+promise
+.then(value => {
+  console.log(value);
+})
+.catch(error => {
+  console.log(error);
+})
+.finally(() => { // 성공 여부 상관없이 무조건.
+  console.log('finally!!!');
+});
+
+// 연결하기
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+.then(num => num * 2)
+.then(num => num * 3)
+.then(num => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(num - 1), 1000);
+  });
+})
+.then(num => console.log(num));
